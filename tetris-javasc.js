2076634267aefb14a2ae1f1cdd4c0b6921125641
Unknown <ania.leonova@gmail.com>
2018-07-05 16:26:46 -0700

    box now stops but how do we reset it to go again

--- tetris-javasc.js
+++ tetris-javasc.js
@@ -6,23 +6,51 @@ var ctx = canvas.getContext("2d");
 var cellWidth = 20;
 var cellHeight = 20;
 
-var rectBeginX = canvas.width/2;
-var rectBeginY = - cellHeight;
+var cellXNum = canvas.width/cellWidth;
+var cellYNum = canvas.height/cellHeight; 
+
+var boxBeginX = canvas.width/2;
+var boxBeginY = -cellHeight;
+
+var cells = [];
+
+for(var c=0; c<canvas.width; c+=20) {
+	cells[c] = [];
+	for(var r=0; r<canvas.height; r+=20) {
+		cells[c][r] = { x: c, y: r, status: 0}; 
+	}
+}
+
+
 
 //FUNCTIONS
 
 
 //draw a box shape
-function drawRect() {
+function drawBox(x, y) {
 	//ctx.clearRect(canvas.width, canvas.height);
 
 	ctx.beginPath();
-	ctx.rect(rectBeginX, rectBeginY, cellWidth, cellHeight);
+	ctx.rect(x, y, cellWidth, cellHeight);
 	ctx.fillStyle = "darkblue";
 	ctx.fill();
 	ctx.closePath();
 
-	rectBeginY += cellWidth;
+	//move it
+	//y += cellWidth;
+
+	if(y == canvas.height) {
+		var ce = cells[x][y-20];
+		//console.log(cells[boxBeginX][400].status);
+		//console.log("the values are"+x + y);
+		//console.log("we reached the bottom");
+		console.log(ce.status); 
+		console.log("success");
+		ce.status = 1;
+
+	}
+
+
 
 }
 
@@ -49,12 +77,34 @@ function drawGrid() {
 
 function mainDraw () {
 
+
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
-	drawRect(); 
+
+		//check for non-empty boxes in cells
+	
+
 	drawGrid();
+
+	drawBox(boxBeginX, boxBeginY-20); 
+
+	boxBeginY += cellHeight;
+
+for(var c=0; c<canvas.width; c+=20) {
+	
+		for(var r=0; r<canvas.height; r+=20) {
+			if(cells[c][r].status == 1) {
+				drawBox(c, r);
+				console.log("status 1");
+				console.log(boxBeginX, boxBeginY);
+
+				//boxBeginY = -cellHeight;
+			}
+		}
+	}
+	
 }
 
 //drawRect();
 
-setInterval(mainDraw, 1000);
+setInterval(mainDraw, 50);
 
