ecb646b3c2ff727bdcd4b8a99e88896fba4a1f1e
Unknown <ania.leonova@gmail.com>
2018-07-05 14:12:55 -0700

    drawing a box

--- tetris-javasc.js
+++ tetris-javasc.js
@@ -6,12 +6,26 @@ var ctx = canvas.getContext("2d");
 var cellWidth = 20;
 var cellHeight = 20;
 
+var rectBeginX = canvas.width/2;
+var rectBeginY = - cellHeight;
+
+//FUNCTIONS
+
+
+//draw a box shape
 function drawRect() {
+	//ctx.clearRect(canvas.width, canvas.height);
+
 	ctx.beginPath();
-	ctx.rect(0, 0, cellWidth, cellHeight);
+	ctx.rect(rectBeginX, rectBeginY, cellWidth, cellHeight);
 	ctx.fillStyle = "darkblue";
 	ctx.fill();
 	ctx.closePath();
+
+	rectBeginY += cellWidth;
+
 }
 
 drawRect();
+
+setInterval(drawRect, 50);
