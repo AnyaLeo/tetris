b67663e2dedbf0dea392f1a82e6d7aa23ae05975
Unknown <ania.leonova@gmail.com>
2018-07-05 13:49:26 -0700

    more js stuff

--- tetris-javasc.js
+++ tetris-javasc.js
@@ -2,3 +2,16 @@
 
 var canvas = document.getElementById("theCanvas");
 var ctx = canvas.getContext("2d");
+
+var cellWidth = 20;
+var cellHeight = 20;
+
+function drawRect() {
+	ctx.beginPath();
+	ctx.rect(0, 0, cellWidth, cellHeight);
+	ctx.fillStyle = "darkblue";
+	ctx.fill();
+	ctx.closePath();
+}
+
+drawRect();
